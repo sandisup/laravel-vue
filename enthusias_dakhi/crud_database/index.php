279349686902574ea358ba49e007a10368121a03
unknown <entusdakhi@gmail.com>
2022-06-16 10:20:33 +0700

    add bootstrap and css on crud db

--- enthusias_dakhi/crud_database/index.php
+++ enthusias_dakhi/crud_database/index.php
@@ -12,23 +12,48 @@
     <title>Homepage</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+    <style>
+        table {
+            padding: 3px;
+            border: 2px;
+        }
+    </style>
 </head>
  
 <body>
 
-<center>
-    <a href="index.php">Buku</a> |
-    <a href="penerbit.php">Penerbit</a> |
-    <a href="pengarang.php">Pengarang</a> |
-    <a href="katalog.php">Katalog</a>
-    <hr>
-</center>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Library</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Buku</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="penerbit.php">Penerbit</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="pengarang.php">Pengarang</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="katalog.php">Katalog</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
 
-<a href="add.php">Add New Buku</a><br/><br/>
- 
+<br/><br/>
+<button type="button" class="btn btn-info" href="add.php">Add New Buku</button>
+<br/><br/>
+
     <table class="table" width='80%' border=1>
  
-    <tr>
+    <tr  style= "text-align: center; background:#006699; color:white" >
         <th>ISBN</th> 
         <th>Judul</th> 
         <th>Tahun</th> 
--- enthusias_dakhi/crud_database/katalog.php
+++ enthusias_dakhi/crud_database/katalog.php
@@ -8,23 +8,47 @@
     <title>Page Katalog</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+    <style>
+        table {
+            padding: 3px;
+            border: 2px;
+        }
+    </style>
+
 </head>
 
 <body>
 
-<center>
-    <a href="index.php">Buku</a> |
-    <a href="penerbit.php">Penerbit</a> |
-    <a href="pengarang.php">Pengarang</a> |
-    <a href="katalog.php">Katalog</a>
-    <hr>
-</center>
-
-<a href="add_katalog.php">Add New Katalog</a><br/><br/>
-
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Library</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Buku</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="penerbit.php">Penerbit</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="pengarang.php">Pengarang</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="katalog.php">Katalog</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<br/><br/>
+<button type="button" class="btn btn-info" href="add_katalog.php">Add New Katalog</button>
+<br/><br/>
 <table class="table" width='80%' border=1>
  
-    <tr>
+<tr  style= "text-align: center; background:#0080B3; color:white" >
         <th>ID Katalog</th> 
         <th>Nama Katalog</th> 
         <th>Aksi</th>
