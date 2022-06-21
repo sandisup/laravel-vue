0d018eb336670533591efdfaad0f812a3ede2caa
unknown <entusdakhi@gmail.com>
2022-06-21 17:47:15 +0700

    membuat relasi model

--- enthusias_dakhi/library/app/Http/Controllers/HomeController.php
+++ enthusias_dakhi/library/app/Http/Controllers/HomeController.php
@@ -2,6 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
+use App\Models\Member;
+use App\Models\Publisher;
+use App\Models\Author;
+use App\Models\Transaction;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +28,14 @@ class HomeController extends Controller
      */
     public function index()
     {
+        //$members = Member::with('user', 'transaction')->get();
+        //$books = Book::with('publisher', 'author', 'catalog')->get();
+        //$publishers = Publisher::with('books')->get();
+        //$authors = Author::with('books')->get();
+        //$catalogs = Catalog::with('books')->get();
+        //$transactions = Transaction::with('transaction_details')->get();
+
+        //return $transactions;
         return view('home');
     }
 }
--- enthusias_dakhi/library/app/Models/Transaction.php
+++ enthusias_dakhi/library/app/Models/Transaction.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Transaction extends Model
 {
     use HasFactory;
+    public function transaction_details()
+    {
+        return $this->hasOne('App\Models\TransactionDetail', 'transaction_id');
+    }
 }
