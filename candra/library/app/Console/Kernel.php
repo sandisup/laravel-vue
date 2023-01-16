b61b840d53b54d89f2cc69113b2985c095d5711e
Your Name <you@example.com>
2023-01-16 22:44:09 +0700

    notification

--- candra/library/app/Console/Kernel.php
+++ candra/library/app/Console/Kernel.php
@@ -4,6 +4,8 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Models\Transaction;
+use App\Models\Notification;
 
 class Kernel extends ConsoleKernel
 {
@@ -16,6 +18,19 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+
+        $schedule->call(function () {
+            //DB::table('recent_users')->delete();
+            $trans = Transaction::where('date_end','<', now())->where('status',0)->get();
+            foreach($trans as $tran){
+                //insert notif
+                Notification::firstOrCreate(
+                    ['transaction_id'=>$tran->id, 
+                     'member_id'=>$tran->member_id,
+                     'message' => 'Peminjaman buku melebihi batas waktu'
+                    ]);
+            }
+        })->everyMinute();
     }
 
     /**
